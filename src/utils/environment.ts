719eb1660b692b3fa457234dff5c2e516eabe9c1
Pancho Mendoza <alu0100846768@ull.edu.es>
2021-09-16 18:46:22 +0100

    created class Env to manage environments variables

---
+++ src/utils/environment.ts
@@ -0,0 +1,38 @@
+import dotenv from 'dotenv';
+
+class Env {
+	constructor(){}
+
+	loadEnvironment(){
+		const envLoadResult = dotenv.config();
+		if (envLoadResult.error) {
+			console.log('Error while load env');
+			process.exit();
+		}
+		checkRequiredVariables();
+	}
+}
+
+// CHECK ENV VARIABLES
+function checkRequiredVariables() {
+	if(process.env.DB_URI == null){
+		console.log('Error: DB_URI is not defined in env');
+		process.exit();
+	}
+
+	if(process.env.DB_USER == null){
+		console.log('Error: DB_USER is not defined in env');
+		process.exit();
+	}
+
+	if(process.env.DB_PASSWORD == null){
+		console.log('Error: DB_PASSWORD is not defined in env');
+		process.exit();
+	}
+
+	if(process.env.PORT == null){
+		console.log('PORT is not defined in env, using defaults');
+	}
+}
+
+export {Env}
